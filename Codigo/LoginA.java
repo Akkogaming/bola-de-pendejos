9e95d90dcdb9b90d36a8350423e49d55da57dde0
Shadow1892 <124979801+Shadow1892@users.noreply.github.com>
2024-08-01 00:18:54 -0700

    Update LoginA.java

--- Codigo/LoginA.java
+++ Codigo/LoginA.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 public class LoginA {
     public static void LoginA() {
-         //Variables
+          //Variables
           Scanner read=new Scanner(System.in);
-          boolean mommyYesmommy=false;
       System.out.println("------------------------------------------------------------");
       System.out.println("|                #LOGIN DEL ADMINISTRADOR#                 |");
+      boolean mommyYesmommy=false;
       do{
         System.out.println("------------------------------------------------------------");
         System.out.println("|                   INGRESE SU NOMBRE                      |");
         System.out.println("------------------------------------------------------------");
-        String name=read.nextLine();
+        String name=read.nextLine().toUpperCase(Locale.getDefault());
         System.out.println("------------------------------------------------------------");
         System.out.println("|                   INGRESE APELLIDO(s)                    |");
         System.out.println("------------------------------------------------------------");
-        String lastName=read.nextLine();
+        String lastName=read.nextLine().toUpperCase(Locale.getDefault());
         if(name.equals("MAURICIO") && lastName.equals("GUERRERO") ){
             System.out.println("------------------------------------------------------------");
             System.out.println("|                   INGRESE CONTRASEÑA                     |");
             System.out.println("------------------------------------------------------------");
-            String password=read.nextLine();
+            String password=read.nextLine().toUpperCase(Locale.getDefault());
                 if(password.equals("URIGOD")){
                 System.out.println("------------------------------------------------------------");
                 System.out.println("|          BIENVENIDO ADMINISTRADOR MAURICIO               |");
                 System.out.println("------------------------------------------------------------");
-                    mommyYesmommy=true;
-                }else{
+                    mommyYesmommy=true;}
+                    else{
                         System.out.println("------------------------------------------------------------");
                         System.out.println("|              NOMBRE O CONTRASEÑA INCORRECTOS             |");
                         System.out.println("|                VUELVA A INGRESAR SUS DATOS               |");
@@ -34,8 +34,8 @@ public class LoginA {
                 }
                     else{
                         System.out.println("------------------------------------------------------------");
-                        System.out.println("-              NOMBRE O CONTRASEÑA INCORRECTOS             -");
-                        System.out.println("-                VUELVA A INGRESAR SUS DATOS               -");
+                        System.out.println("|              NOMBRE O CONTRASEÑA INCORRECTOS             |");
+                        System.out.println("|                VUELVA A INGRESAR SUS DATOS               |");
                         System.out.println("------------------------------------------------------------");
                             }
     }while(mommyYesmommy==false);
