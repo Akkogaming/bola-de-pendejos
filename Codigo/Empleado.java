b5bd09d1b4a01dbce41c2b20492eee7240b1cc0c
Shadow1892 <124979801+Shadow1892@users.noreply.github.com>
2024-07-31 21:58:55 -0700

    Empleado.java

---
+++ Codigo/Empleado.java
@@ -0,0 +1,30 @@
+public class Empleado{
+
+  //Variables
+  private name;
+  private firstSurname;
+  private secondSurname;
+  private eMail;
+
+  //Methods
+  public Empleado(String name,String firstSurname,String secondSurname,String eMail){
+  }
+
+  public Empleado(){}
+
+  public void setName(String name){
+    this.name=name;
+  }
+
+    public void setFirstSurname(String firstSurname){
+    this.firstSurname=firstSurname;
+  }
+
+    public void setSecondSurname(String secondSurname){
+    this.secondSurname=secondSurname;
+  }
+
+    public void setEmail(String eMail){
+    this.eMail=eMail;
+  }
+}
