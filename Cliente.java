553b4b946fe0731b3c31c6e22e77878e1494dea7
Shadow1892 <124979801+Shadow1892@users.noreply.github.com>
2024-07-31 21:50:03 -0700

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente extends Empleado{
+
+  //Variables
+  private String phone;
+  
+  //Methods
+  public void setPhone(String phone){
+    this.phone=phone;
+  }
+
+  public String getPhone(){
+    return phone;
+  }
+}
