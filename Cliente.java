b7b922f375ca8e67a3e3bf631e4715180d95aa92
Shadow1892 <124979801+Shadow1892@users.noreply.github.com>
2024-07-31 22:04:29 -0700

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,14 +1,3 @@
 public class Cliente extends Empleado{
 
-  //Variables
-  private String phone;
-  
-  //Methods
-  public void setPhone(String phone){
-    this.phone=phone;
-  }
-
-  public String getPhone(){
-    return phone;
-  }
 }
