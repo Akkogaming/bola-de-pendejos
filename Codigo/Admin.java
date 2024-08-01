f9d4f7fa91ff4bd541282d0bd7485427ac02ca05
Shadow1892 <124979801+Shadow1892@users.noreply.github.com>
2024-08-01 00:39:59 -0700

    Update Admin.java

--- Codigo/Admin.java
+++ Codigo/Admin.java
@@ -1,3 +1,21 @@
 public class Admin{
-  
+        System.out.println("------------------------------------------------------------");
+        System.out.println("|                   ¿QUÉ DESEA REALIZAR?                   |");
+        System.out.println("|             A.CONSULTAS  B.ELIMINAR EMPLEADOS            |");
+        System.out.println("|             C.AÑADIR SALONES D.AÑADIR EMPLEADOS          |");
+        System.out.println("|             E.ELIMINAR SALONES F.AÑADIR SERVICIOS        |");
+        System.out.println("|             G.ELIMINAR SERVICIOS  H.AÑADIR MONTAJES      |");
+        System.out.println("|             I.ELIMINAR MONTAJES  J.AÑADIR EVENTOS        |");
+        System.out.println("|             K.ELIMINAR EVENTOS  L.SALIR                  |");
+        System.out.println("------------------------------------------------------------");
+        String answer=read.nextLine().toUpperCase(Locale.getDefault());
+
+        switch(answer){
+          case "A":
+            Consultas consultProgram=new Consultas();
+            break;
+
+          case "B":
+            
+        }
 }
