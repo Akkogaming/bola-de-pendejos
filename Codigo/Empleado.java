23917a49cabdc49a2af43a683699a6601c1eeece
Raccoon64-c <mizahel.roy.6464@gmail.com>
2024-08-08 07:32:29 -0700

    payaso dorado

--- Codigo/Empleado.java
+++ Codigo/Empleado.java
@@ -93,87 +93,112 @@ public class Empleado {
   }
   
   public static void ReservarSalon(){
-    try{
-      boolean crearReservacion;
-      int codigoSalon;
-      int codigocliente;
-      int cant_inv;
-      int evento;
-      int montaje;
-      int equipamiento;
-      int servicio;
-      float monto=4455;
-      float montoT=5541;
-      do{
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║             Creando una nueva reservacion                    ║");
-        System.out.println("║            Quien fue el que le pidio el salon?               ║");
-        System.out.println("║          Favor de ingresar el codigo del cliente             ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        codigocliente=leer.nextInt();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║                 Que salon se va a requerir?                  ║");
-        System.out.println("║             Favor de introducir el codigo del Salon          ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        codigoSalon=leer.nextInt();
-        leer.nextLine(); 
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║            A que hora necesita el cliente ese salon?         ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        String horaInicio=leer.nextLine();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║             A que hora se desocupara el salon?               ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        String horaFinal=leer.nextLine();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║      Cual es la fecha en la que se requerira el salon?       ║");
-        System.out.println("║      Por favor escriba en el siguiente orden: AA/MM/DD       ║");
-        System.out.println("║              Favor de solo introducir numeros                ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        String Fecha=leer.nextLine();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║          Cuantos invitados asistiran al evento?              ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        cant_inv=leer.nextInt();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║        Que tipo de servicio ha pedido el cliente?            ║");
-        System.out.println("║        Favor de introducir el codigo del servicio            ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        servicio=leer.nextInt();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║      Ingrese el codigo del evento que se solicitara          ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        evento=leer.nextInt();
-
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║     Ingrese el codigo del montaje que se realizara           ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        montaje=leer.nextInt();
-
-        System.out.println("╔════════════════════════════════════════════════════════════════╗");
-        System.out.println("║Por ultimo, ingrese el codigo del equipamiento que se requerira ║");
-        System.out.println("╚════════════════════════════════════════════════════════════════╝");
-        equipamiento=leer.nextInt();
-        
-        String comando = "INSERT INTO reservaciones(fechaevento, Cant_Inv, HoraI, HoraF, monto, montoT, cliente, salon, evento) VALUES('"+
-        Fecha+"','"+cant_inv+"','"+horaInicio+"','"+horaFinal+"','"+monto+"','"+montoT+"','"+codigocliente+"','"+codigoSalon+"','"+evento+"')";
-        statement.executeUpdate(comando);
-        System.out.println("╔══════════════════════════════════════════════════════════════╗");
-        System.out.println("║              Reservacion creada con exito                    ║");
-        System.out.println("╚══════════════════════════════════════════════════════════════╝");
-        crearReservacion=false;
-      }while(crearReservacion);
-    }catch(Exception e){
-      e.printStackTrace();
+      try{
+        boolean crearReservacion;
+        int codigoSalon;
+        int codigocliente;
+        int cant_inv;
+        int evento;
+        int equipamiento;
+        int servicio;
+        double monto;
+        double montoT;
+        do{
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║             Creando una nueva reservacion                    ║");
+          System.out.println("║            Quien fue el que le pidio el salon?               ║");
+          System.out.println("║          Favor de ingresar el codigo del cliente             ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          codigocliente=leer.nextInt();
+          leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║                 Que salon se va a requerir?                  ║");
+          System.out.println("║             Favor de introducir el codigo del Salon          ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          codigoSalon=leer.nextInt();
+          leer.nextLine(); 
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║            A que hora necesita el cliente ese salon?         ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          String horaInicio=leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║             A que hora se desocupara el salon?               ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          String horaFinal=leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║      Cual es la fecha en la que se requerira el salon?       ║");
+          System.out.println("║      Por favor escriba en el siguiente orden: AA/MM/DD       ║");
+          System.out.println("║              Favor de solo introducir numeros                ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          String Fecha=leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║          Cuantos invitados asistiran al evento?              ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          cant_inv=leer.nextInt();
+          leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║        Que tipo de servicio ha pedido el cliente?            ║");
+          System.out.println("║        Favor de introducir el codigo del servicio            ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          servicio=leer.nextInt();
+          leer.nextLine();
+  
+          System.out.println("╔══════════════════════════════════════════════════════════════╗");
+          System.out.println("║      Ingrese el codigo del evento que se solicitara          ║");
+          System.out.println("╚══════════════════════════════════════════════════════════════╝");
+          evento=leer.nextInt();
+  
+          System.out.println("╔════════════════════════════════════════════════════════════════╗");
+          System.out.println("║Por ultimo, ingrese el codigo del equipamiento que se requerira ║");
+          System.out.println("╚════════════════════════════════════════════════════════════════╝");
+          equipamiento=leer.nextInt();
+          
+          // Ejecutar consultas para obtener los precios de la base de datos
+          String consultaServicio = "SELECT Precio FROM servicios WHERE codigo = ?";
+          PreparedStatement psServicio = connect.prepareStatement(consultaServicio);
+          psServicio.setInt(1, servicio);
+          ResultSet rsServicio = psServicio.executeQuery();
+          rsServicio.next();
+          double precioServicio = rsServicio.getFloat("Precio");
+  
+          String consultaTipoEvento = "SELECT precio FROM tipo_evento WHERE codigo = ?";
+          PreparedStatement psTipoEvento = connect.prepareStatement(consultaTipoEvento);
+          psTipoEvento.setInt(1, evento);
+          ResultSet rsEvento = psTipoEvento.executeQuery();
+          rsEvento.next();
+          double precioMontaje = rsEvento.getFloat("precio");
+  
+          String consultarEquipamiento = "SELECT precio FROM equipamiento WHERE codigo = ?";
+          PreparedStatement psEquipamiento = connect.prepareStatement(consultarEquipamiento);
+          psEquipamiento.setInt(1, equipamiento);
+          ResultSet rsEquipamiento = psEquipamiento.executeQuery();
+          rsEquipamiento.next();
+          double precioEquipamiento = rsEquipamiento.getFloat("precio");
+  
+            // Calcular monto y montoT
+            monto = precioServicio + precioMontaje + precioEquipamiento;
+            montoT = monto * (1.16); // Asumiendo 16% del impuesto
+  
+            // Now you can use these calculated values in your INSERT statement
+            String comando = "INSERT INTO reservaciones(fechaevento, Cant_Inv, HoraI, HoraF, monto, montoT, cliente, salon, evento) VALUES('"+
+            Fecha+"','"+cant_inv+"','"+horaInicio+"','"+horaFinal+"','"+monto+"','"+montoT+"','"+codigocliente+"','"+codigoSalon+"','"+evento+"')";
+            statement.executeUpdate(comando);
+            System.out.println("╔══════════════════════════════════════════════════════════════╗");
+            System.out.println("║              Reservacion creada con exito                    ║");
+            System.out.println("╚══════════════════════════════════════════════════════════════╝");
+            crearReservacion=false;
+        }while(crearReservacion);
+      }catch(Exception e){
+        e.printStackTrace();
+      }
     }
-  }
+  
   public static void eliminarReservacion(){
     try{
         System.out.println("Número de código a borrar:");
